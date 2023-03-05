77ac876d5cd6769bb0bdea4296c8c7cef8a1db6b
Baramex <contact@baramex.me>
2023-03-05 20:20:05 +0100

    settled up project with content react script

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Main extends React.Component {
+    render() {
+        return (
+            <div className={'my-extension'}>
+                <h1>Hello world - My first Extension</h1>
+            </div>
+        )
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = document.createElement('div');
+app.id = "my-extension-root";
+document.body.appendChild(app);
+ReactDOM.render(<Main />, app);
